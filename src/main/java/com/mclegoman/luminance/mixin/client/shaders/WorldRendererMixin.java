82dc87812156ac8015a940d45fe4a2307bce336d
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2025-01-08 19:30:04 +0000

    fixed transparent objects not rendering in fast/fancy

--- src/main/java/com/mclegoman/luminance/mixin/client/shaders/WorldRendererMixin.java
+++ src/main/java/com/mclegoman/luminance/mixin/client/shaders/WorldRendererMixin.java
@@ -11,18 +11,20 @@ import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import com.mclegoman.luminance.client.events.Execute;
+import com.mclegoman.luminance.client.shaders.PersistentFramebufferFactory;
 import com.mclegoman.luminance.client.shaders.interfaces.FramePassInterface;
+import com.mclegoman.luminance.common.data.Data;
 import net.minecraft.client.gl.Framebuffer;
 import net.minecraft.client.gl.PostEffectProcessor;
 import net.minecraft.client.gl.SimpleFramebufferFactory;
 import net.minecraft.client.render.*;
 import net.minecraft.client.util.ObjectAllocator;
 import net.minecraft.client.util.Pool;
+import net.minecraft.util.Identifier;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -37,27 +39,20 @@ public abstract class WorldRendererMixin {
 	}
 
 	@Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/FrameGraphBuilder;createPass(Ljava/lang/String;)Lnet/minecraft/client/render/RenderPass;"))
-	private void luminance$enableFramebuffers(ObjectAllocator allocator, RenderTickCounter tickCounter, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f positionMatrix, Matrix4f projectionMatrix, CallbackInfo ci, @Local FrameGraphBuilder frameGraphBuilder, @Local SimpleFramebufferFactory simpleFramebufferFactory, @Local PostEffectProcessor postEffectProcessor) {
-		if (postEffectProcessor == null) {
-			this.framebufferSet.translucentFramebuffer = frameGraphBuilder.createResourceHandle("translucent", simpleFramebufferFactory);
-			this.framebufferSet.itemEntityFramebuffer = frameGraphBuilder.createResourceHandle("item_entity", simpleFramebufferFactory);
-			this.framebufferSet.particlesFramebuffer = frameGraphBuilder.createResourceHandle("particles", simpleFramebufferFactory);
-			this.framebufferSet.weatherFramebuffer = frameGraphBuilder.createResourceHandle("weather", simpleFramebufferFactory);
-			this.framebufferSet.cloudsFramebuffer = frameGraphBuilder.createResourceHandle("clouds", simpleFramebufferFactory);
-		}
+	private void luminance$copyFramebuffer(ObjectAllocator allocator, RenderTickCounter tickCounter, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f positionMatrix, Matrix4f projectionMatrix, CallbackInfo ci, @Local FrameGraphBuilder frameGraphBuilder, @Local SimpleFramebufferFactory simpleFramebufferFactory, @Local PostEffectProcessor postEffectProcessor, @Share("factory") LocalRef<SimpleFramebufferFactory> factory) {
+		factory.set(simpleFramebufferFactory);
 	}
 
 	@Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gl/PostEffectProcessor;render(Lnet/minecraft/client/render/FrameGraphBuilder;IILnet/minecraft/client/gl/PostEffectProcessor$FramebufferSet;)V", ordinal = 1))
-	private void luminance$copyDepth(ObjectAllocator allocator, RenderTickCounter tickCounter, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f positionMatrix, Matrix4f projectionMatrix, CallbackInfo ci, @Local FrameGraphBuilder frameGraphBuilder, @Local SimpleFramebufferFactory simpleFramebufferFactory, @Share("depthBackup") LocalRef<Framebuffer> depthBackup, @Share("factory") LocalRef<SimpleFramebufferFactory> factory) {
+	private void luminance$copyDepth(ObjectAllocator allocator, RenderTickCounter tickCounter, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f positionMatrix, Matrix4f projectionMatrix, CallbackInfo ci, @Local FrameGraphBuilder frameGraphBuilder, @Share("depthBackup") LocalRef<Framebuffer> depthBackup, @Share("factory") LocalRef<SimpleFramebufferFactory> factory) {
 		RenderPass renderPass = frameGraphBuilder.createPass("copyDepth");
 		((FramePassInterface)renderPass).luminance$setForceVisit(true);
 
 		renderPass.setRenderer(() -> {
 			Pool pool = ((GameRendererAccessor)gameRenderer).getPool();
-			depthBackup.set(pool.acquire(simpleFramebufferFactory));
+			depthBackup.set(pool.acquire(factory.get()));
 			depthBackup.get().copyDepthFrom(framebufferSet.mainFramebuffer.get());
 		});
-		factory.set(simpleFramebufferFactory);
 	}
 
 	@Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gl/PostEffectProcessor;render(Lnet/minecraft/client/render/FrameGraphBuilder;IILnet/minecraft/client/gl/PostEffectProcessor$FramebufferSet;)V", ordinal = 1, shift = At.Shift.AFTER))
@@ -73,22 +68,24 @@ public abstract class WorldRendererMixin {
 	}
 
 	@Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/WorldRenderer;renderLateDebug(Lnet/minecraft/client/render/FrameGraphBuilder;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/client/render/Fog;)V", shift = At.Shift.AFTER))
-	private void luminance$afterRenderWeather(ObjectAllocator allocator, RenderTickCounter tickCounter, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f positionMatrix, Matrix4f projectionMatrix, CallbackInfo ci, @Local FrameGraphBuilder frameGraphBuilder, @Share("depthBackup") LocalRef<Framebuffer> depthBackup) {
-		//if (postEffectProcessor == null) {
-		//	this.framebufferSet.translucentFramebuffer = frameGraphBuilder.createResourceHandle("translucent", simpleFramebufferFactory);
-		//	this.framebufferSet.itemEntityFramebuffer = frameGraphBuilder.createResourceHandle("item_entity", simpleFramebufferFactory);
-		//	this.framebufferSet.particlesFramebuffer = frameGraphBuilder.createResourceHandle("particles", simpleFramebufferFactory);
-		//	this.framebufferSet.weatherFramebuffer = frameGraphBuilder.createResourceHandle("weather", simpleFramebufferFactory);
-		//	this.framebufferSet.cloudsFramebuffer = frameGraphBuilder.createResourceHandle("clouds", simpleFramebufferFactory);
-		//}
-		Execute.afterWeatherRender(frameGraphBuilder, this.framebufferSet);
-		//if (postEffectProcessor == null) {
-		//	this.framebufferSet.translucentFramebuffer = null;
-		//	this.framebufferSet.itemEntityFramebuffer = null;
-		//	this.framebufferSet.particlesFramebuffer = null;
-		//	this.framebufferSet.weatherFramebuffer = null;
-		//	this.framebufferSet.cloudsFramebuffer = null;
-		//}
+	private void luminance$afterRenderWeather(ObjectAllocator allocator, RenderTickCounter tickCounter, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f positionMatrix, Matrix4f projectionMatrix, CallbackInfo ci, @Local FrameGraphBuilder frameGraphBuilder, @Share("factory") LocalRef<SimpleFramebufferFactory> factory) {
+		boolean replace = framebufferSet.translucentFramebuffer == null;
+		if (replace) {
+			PersistentFramebufferFactory persistentFramebufferFactory = new PersistentFramebufferFactory(factory.get(), null, Identifier.of(Data.version.getID(), "fabulous"));
+			framebufferSet.translucentFramebuffer = frameGraphBuilder.createResourceHandle("translucent", persistentFramebufferFactory);
+			framebufferSet.itemEntityFramebuffer = frameGraphBuilder.createResourceHandle("item_entity", persistentFramebufferFactory);
+			framebufferSet.particlesFramebuffer = frameGraphBuilder.createResourceHandle("particles", persistentFramebufferFactory);
+			framebufferSet.weatherFramebuffer = frameGraphBuilder.createResourceHandle("weather", persistentFramebufferFactory);
+			framebufferSet.cloudsFramebuffer = frameGraphBuilder.createResourceHandle("clouds", persistentFramebufferFactory);
+		}
+		Execute.afterWeatherRender(frameGraphBuilder, framebufferSet);
+		if (replace) {
+			framebufferSet.translucentFramebuffer = null;
+			framebufferSet.itemEntityFramebuffer = null;
+			framebufferSet.particlesFramebuffer = null;
+			framebufferSet.weatherFramebuffer = null;
+			framebufferSet.cloudsFramebuffer = null;
+		}
 	}
 
 	@Inject(method = "render", at = @At("TAIL"))
