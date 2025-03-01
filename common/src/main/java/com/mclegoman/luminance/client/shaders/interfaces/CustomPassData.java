04680fbf38079bfc5086de5cd54d3f098aebd0e7
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2025-03-01 21:24:53 +0000

    added implementation for CustomPassData

--- common/src/main/java/com/mclegoman/luminance/client/shaders/interfaces/CustomPassData.java
+++ common/src/main/java/com/mclegoman/luminance/client/shaders/interfaces/CustomPassData.java
@@ -1,5 +1,23 @@
 package com.mclegoman.luminance.client.shaders.interfaces;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public interface CustomPassData {
     CustomPassData copy();
+
+    class CustomPassDataMap<K,V> extends HashMap<K,V> implements CustomPassData {
+        public CustomPassDataMap(int initialCapacity) {
+            super(initialCapacity);
+        }
+
+        public CustomPassDataMap(Map<? extends K, ? extends V> m) {
+            super(m);
+        }
+
+        @Override
+        public CustomPassData copy() {
+            return new CustomPassDataMap<>(this);
+        }
+    }
 }
