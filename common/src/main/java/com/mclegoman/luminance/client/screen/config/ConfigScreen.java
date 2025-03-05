4e353481c2137079945110315642800025fb7480
dannytaylor <daniel@mclegoman.com>
2025-03-06 00:05:03 +1300

    clear children on init.

--- common/src/main/java/com/mclegoman/luminance/client/screen/config/ConfigScreen.java
+++ common/src/main/java/com/mclegoman/luminance/client/screen/config/ConfigScreen.java
@@ -73,7 +73,7 @@ public class ConfigScreen extends Screen {
 		this(parent, false, false, null, isPride);
 	}
 	public void init() {
-		super.init();
+		clearChildren();
 		try {
 			grid.getMainPositioner().alignHorizontalCenter().margin(0);
 			GridWidget.Adder gridAdder = grid.createAdder(1);
