e65acdda57d633a59abf98bf98719e95d77c25c2
dannytaylor <daniel@mclegoman.com>
2025-08-27 18:39:37 +1200

    added abstract widget list screen

---
+++ src/main/java/com/mclegoman/luminance/client/gui/screen/AbstractScrollableListScreen.java
@@ -0,0 +1,47 @@
+/*
+    Luminance
+    Contributor(s): dannytaylor
+    Github: https://github.com/mclegoman/Luminance
+    Licence: GNU LGPLv3
+*/
+
+package com.mclegoman.luminance.client.gui.screen;
+
+import com.mclegoman.luminance.client.data.ClientData;
+import com.mclegoman.luminance.client.gui.widget.ListWidget;
+import com.mclegoman.luminance.client.translation.Translation;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ClickableWidget;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractScrollableListScreen extends AbstractScrollableScreen {
+	public ListWidget list;
+
+	public AbstractScrollableListScreen(Screen parent) {
+		super(parent);
+	}
+
+	public AbstractScrollableListScreen(Screen parent, double scrollY) {
+		super(parent, scrollY);
+	}
+
+	public AbstractScrollableListScreen(String id, Screen parent) {
+		super(id, parent);
+	}
+
+	public AbstractScrollableListScreen(String id, Screen parent, double scrollY, Translation.@Nullable Data splashText, boolean isPride) {
+		super(id, parent, scrollY, splashText, isPride);
+	}
+
+	public void initBody() {
+		this.list = new ListWidget(ClientData.minecraft, this.width, this.layout.getContentHeight(), this.layout.getHeaderHeight(), 24, getWidgets(), this.scrollY);
+		this.layout.addBody(this.list);
+	}
+
+	public List<ClickableWidget> getWidgets() {
+		return new ArrayList<>();
+	}
+}
