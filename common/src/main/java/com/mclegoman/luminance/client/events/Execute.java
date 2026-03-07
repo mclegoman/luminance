2d5fd1066f7db32c6780d9b91bdc94e4c5307276
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2026-03-07 21:13:57 +0000

    fixes for things in Execute

--- common/src/main/java/com/mclegoman/luminance/client/events/Execute.java
+++ common/src/main/java/com/mclegoman/luminance/client/events/Execute.java
@@ -17,7 +17,6 @@ import com.mclegoman.luminance.client.translation.Translation;
 import com.mclegoman.luminance.client.util.CompatHelper;
 import com.mclegoman.luminance.common.data.Data;
 import com.mclegoman.luminance.common.util.LogType;
-import com.mojang.blaze3d.systems.ProjectionType;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gl.*;
 import net.minecraft.client.gui.DrawContext;
@@ -27,7 +26,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.resource.ReloadableResourceManagerImpl;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.NotNull;
-import org.joml.Matrix4f;
+import org.lwjgl.opengl.GL11;
 
 public class Execute {
 	public static void registerClientResourceReloaders(ReloadableResourceManagerImpl resourceManager) {
@@ -83,7 +82,12 @@ public class Execute {
 	public static void afterVanillaPostEffectRender(ObjectAllocator allocator) {
 		mergeDepth(allocator);
 
-		RenderSystem.depthMask(false);
+		// direct GL call to replace RenderSystem.depthMask. not sure if theres an api better alternative
+		// vulkan seems to do it by having a separate pipeline? perhaps the pipeline used for shaders can have their depth disabled?
+
+		// TODO: see if this has been done already
+
+		GL11.glDepthMask(false);
 		Events.AfterVanillaPostEffectRender.registry.forEach(((id, runnable) -> {
 			try {
 				runnable.run(ClientData.minecraft.getFramebuffer(), allocator);
@@ -91,7 +95,7 @@ public class Execute {
 				Data.getVersion().sendToLog(LogType.ERROR, Translation.getString("Failed to execute VanillaPostEffect event with id: {}: {}", id, error));
 			}
 		}));
-		RenderSystem.depthMask(true);
+		GL11.glDepthMask(true);
 	}
 	public static void afterUiRender(ObjectAllocator allocator) {
 		Events.AfterUiRender.registry.forEach(((id, runnable) -> {
@@ -145,7 +149,8 @@ public class Execute {
 			return;
 		}
 
-		FramePassInterface.createForcedPass(frameGraphBuilder, Identifier.of(Data.getVersion().getID(), "prepare_shader_render"), () -> RenderSystem.depthMask(false));
+		// see Execute.afterVanillaPostEffectRender() for note on depth mask
+		FramePassInterface.createForcedPass(frameGraphBuilder, Identifier.of(Data.getVersion().getID(), "prepare_shader_render"), () -> GL11.glDepthMask(false));
 		Events.AfterFabulousRender.registry.forEach(((id, runnable) -> {
 			try {
 				runnable.run(frameGraphBuilder, ClientData.minecraft.getFramebuffer().textureWidth, ClientData.minecraft.getFramebuffer().textureHeight, framebufferSet);
@@ -153,7 +158,7 @@ public class Execute {
 				Data.getVersion().sendToLog(LogType.ERROR, Translation.getString("Failed to execute AfterFabulousRender event with id: {}: {}", id, error));
 			}
 		}));
-		FramePassInterface.createForcedPass(frameGraphBuilder, Identifier.of(Data.getVersion().getID(), "cleanup_shader_render"), () -> RenderSystem.depthMask(true));
+		FramePassInterface.createForcedPass(frameGraphBuilder, Identifier.of(Data.getVersion().getID(), "cleanup_shader_render"), () -> GL11.glDepthMask(true));
 	}
 	public static void afterWorldRender(ObjectAllocator allocator) {
 		Events.AfterWorldRender.registry.forEach(((id, runnable) -> {
@@ -185,8 +190,8 @@ public class Execute {
 		}));
 	}
 
-	private static SimpleFramebufferFactory framebufferFactory;
-	private static Framebuffer worldDepth;
+	//private static SimpleFramebufferFactory framebufferFactory;
+	//private static Framebuffer worldDepth;
 
 	private static void copyDepth(ObjectAllocator allocator) {
 		cleanupDepth(allocator);
@@ -195,13 +200,16 @@ public class Execute {
 			return;
 		}
 
-		Framebuffer framebuffer = ClientData.minecraft.getFramebuffer();
+		// TODO: update this (its used for depth merging)
+		//  beginWrite no longer exists, it might be fine to just ignore though, it might rebind the correct buffer as is
 
-		framebufferFactory = new SimpleFramebufferFactory(framebuffer.textureWidth, framebuffer.textureHeight, true, 0);
-		worldDepth = allocator.acquire(framebufferFactory);
-		worldDepth.copyDepthFrom(framebuffer);
+		//Framebuffer framebuffer = ClientData.minecraft.getFramebuffer();
 
-		framebuffer.beginWrite(false);
+		//framebufferFactory = new SimpleFramebufferFactory(framebuffer.textureWidth, framebuffer.textureHeight, true, 0);
+		//worldDepth = allocator.acquire(framebufferFactory);
+		//worldDepth.copyDepthFrom(framebuffer);
+
+		//framebuffer.beginWrite(false);
 	}
 
 	private static void mergeDepth(ObjectAllocator allocator) {
@@ -210,33 +218,37 @@ public class Execute {
 		}
 
 		try {
-			Framebuffer framebuffer = ClientData.minecraft.getFramebuffer();
-			framebuffer.beginWrite(true);
+			// TODO: update this
+			//  i dont remember how i went around creating this
+			//  i think i fully reverse engineered how shader loading works myself
 
-			ShaderProgram shaderProgram = ClientData.minecraft.getShaderLoader().getProgramToLoad(new ShaderProgramKey(Identifier.of(Data.getVersion().getID(), "depth_fix"), VertexFormats.POSITION, Defines.EMPTY));
-			shaderProgram.addSamplerTexture("InSampler", worldDepth.getDepthAttachment());
-			shaderProgram.addSamplerTexture("HandSampler", framebuffer.getDepthAttachment());
-			shaderProgram.getUniformOrDefault("InSize").set((float)framebuffer.textureWidth, (float)framebuffer.textureHeight);
-			shaderProgram.getUniformOrDefault("OutSize").set((float)framebuffer.textureWidth, (float)framebuffer.textureHeight);
-			RenderSystem.setShader(shaderProgram);
-
-			RenderSystem.depthFunc(519);
-			RenderSystem.enableDepthTest();
-			RenderSystem.depthMask(true);
-
-			Matrix4f projectionMatrix = (new Matrix4f()).setOrtho(0.0F, (float)framebuffer.textureWidth, 0.0F, (float)framebuffer.textureHeight, 0.1F, 1000.0F);
-
-			RenderSystem.backupProjectionMatrix();
-			RenderSystem.setProjectionMatrix(projectionMatrix, ProjectionType.ORTHOGRAPHIC);
-			BufferBuilder bufferBuilder = Tessellator.getInstance().begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION);
-			bufferBuilder.vertex(0.0F, 0.0F, 500.0F);
-			bufferBuilder.vertex((float)framebuffer.textureWidth, 0.0F, 500.0F);
-			bufferBuilder.vertex((float)framebuffer.textureWidth, (float)framebuffer.textureHeight, 500.0F);
-			bufferBuilder.vertex(0.0F, (float)framebuffer.textureHeight, 500.0F);
-			BufferRenderer.drawWithGlobalProgram(bufferBuilder.end());
-			RenderSystem.restoreProjectionMatrix();
-
-			framebuffer.endWrite();
+//			Framebuffer framebuffer = ClientData.minecraft.getFramebuffer();
+//			framebuffer.beginWrite(true);
+//
+//			ShaderProgram shaderProgram = ClientData.minecraft.getShaderLoader().getProgramToLoad(new ShaderProgramKey(Identifier.of(Data.getVersion().getID(), "depth_fix"), VertexFormats.POSITION, Defines.EMPTY));
+//			shaderProgram.addSamplerTexture("InSampler", worldDepth.getDepthAttachment());
+//			shaderProgram.addSamplerTexture("HandSampler", framebuffer.getDepthAttachment());
+//			shaderProgram.getUniformOrDefault("InSize").set((float)framebuffer.textureWidth, (float)framebuffer.textureHeight);
+//			shaderProgram.getUniformOrDefault("OutSize").set((float)framebuffer.textureWidth, (float)framebuffer.textureHeight);
+//			RenderSystem.setShader(shaderProgram);
+//
+//			RenderSystem.depthFunc(519);
+//			RenderSystem.enableDepthTest();
+//			RenderSystem.depthMask(true);
+//
+//			Matrix4f projectionMatrix = (new Matrix4f()).setOrtho(0.0F, (float)framebuffer.textureWidth, 0.0F, (float)framebuffer.textureHeight, 0.1F, 1000.0F);
+//
+//			RenderSystem.backupProjectionMatrix();
+//			RenderSystem.setProjectionMatrix(projectionMatrix, ProjectionType.ORTHOGRAPHIC);
+//			BufferBuilder bufferBuilder = Tessellator.getInstance().begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION);
+//			bufferBuilder.vertex(0.0F, 0.0F, 500.0F);
+//			bufferBuilder.vertex((float)framebuffer.textureWidth, 0.0F, 500.0F);
+//			bufferBuilder.vertex((float)framebuffer.textureWidth, (float)framebuffer.textureHeight, 500.0F);
+//			bufferBuilder.vertex(0.0F, (float)framebuffer.textureHeight, 500.0F);
+//			BufferRenderer.drawWithGlobalProgram(bufferBuilder.end());
+//			RenderSystem.restoreProjectionMatrix();
+//
+//			framebuffer.endWrite();
 		} catch (Exception e) {
 			Data.getVersion().sendToLog(LogType.INFO, "Error Fixing Depth: "+e.getMessage());
 		}
@@ -245,9 +257,9 @@ public class Execute {
 	}
 
 	private static void cleanupDepth(ObjectAllocator allocator) {
-		if (worldDepth != null) {
-			allocator.release(framebufferFactory, worldDepth);
-			worldDepth = null;
-		}
+		// if (worldDepth != null) {
+		// 	allocator.release(framebufferFactory, worldDepth);
+		// 	worldDepth = null;
+		// }
 	}
 }
