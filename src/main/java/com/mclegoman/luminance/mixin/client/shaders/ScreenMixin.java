298109cde858afebab61241ab3959e456acf793e
dannytaylor <me@dannytaylor.dev>
2026-04-04 17:02:45 +1300

    ui background when in world but not in a game gui

--- src/main/java/com/mclegoman/luminance/mixin/client/shaders/ScreenMixin.java
+++ src/main/java/com/mclegoman/luminance/mixin/client/shaders/ScreenMixin.java
@@ -18,13 +18,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(priority = 100, value = Screen.class)
 public abstract class ScreenMixin {
+	@Inject(method = "renderBackground", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;applyBlur(Lnet/minecraft/client/gui/DrawContext;)V"))
+	private void luminance$afterBackgroundRender_inWorldBeforeBlur(DrawContext context, int mouseX, int mouseY, float deltaTicks, CallbackInfo ci) {
+		if (ClientData.minecraft.world != null) Execute.afterUiBackgroundRender(((GameRendererAccessor) ClientData.minecraft.gameRenderer).getPool());
+	}
+
 	@Inject(method = "renderInGameBackground", at = @At("RETURN"))
-	private void luminance$afterBackgroundRender_afterGradient(DrawContext context, CallbackInfo ci) {
-		Execute.afterUiBackgroundRender(((GameRendererAccessor) ClientData.minecraft.gameRenderer).getPool());
+	private void luminance$afterBackgroundRender_afterInGameBackground(DrawContext context, CallbackInfo ci) {
+		if (ClientData.minecraft.world != null) Execute.afterUiBackgroundRender(((GameRendererAccessor) ClientData.minecraft.gameRenderer).getPool());
 	}
 
 	@Inject(method = "renderPanoramaBackground", at = @At("RETURN"))
-	private void luminance$afterBackgroundRender_afterPanorama(DrawContext context, float deltaTicks, CallbackInfo ci) {
-		Execute.afterUiBackgroundRender(((GameRendererAccessor) ClientData.minecraft.gameRenderer).getPool());
+	private void luminance$afterBackgroundRender_notInWorld(DrawContext context, float deltaTicks, CallbackInfo ci) {
+		if (ClientData.minecraft.world == null) Execute.afterUiBackgroundRender(((GameRendererAccessor) ClientData.minecraft.gameRenderer).getPool());
 	}
 }
