4ca4dae1619326c8519e51ae18c1be06e5c083f9
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2026-04-16 19:00:01 +0100

    removed unneeded depth copy

--- src/main/java/com/mclegoman/luminance/mixin/client/shaders/LevelRendererMixin.java
+++ src/main/java/com/mclegoman/luminance/mixin/client/shaders/LevelRendererMixin.java
@@ -10,23 +10,16 @@ package com.mclegoman.luminance.mixin.client.shaders;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
-import com.mclegoman.luminance.client.data.ClientData;
 import com.mclegoman.luminance.client.events.Execute;
 import com.mclegoman.luminance.client.shaders.LuminanceTargetBundle;
-import com.mclegoman.luminance.client.shaders.interfaces.FramePassInterface;
-import com.mclegoman.luminance.common.data.Data;
 import com.mojang.blaze3d.buffers.GpuBufferSlice;
 import com.mojang.blaze3d.framegraph.FrameGraphBuilder;
-import com.mojang.blaze3d.pipeline.RenderTarget;
 import net.minecraft.client.Camera;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.LevelTargetBundle;
-import net.minecraft.client.renderer.PostChain;
 import com.mojang.blaze3d.resource.RenderTargetDescriptor;
 import com.mojang.blaze3d.resource.GraphicsResourceAllocator;
-import com.mojang.blaze3d.resource.CrossFrameResourcePool;
-import net.minecraft.resources.Identifier;
 import org.joml.Matrix4f;
 import org.joml.Vector4f;
 import org.spongepowered.asm.mixin.Final;
@@ -43,30 +36,6 @@ public abstract class LevelRendererMixin {
 	@Inject(method = "renderLevel", at = @At("HEAD"))
 	private void luminance$beforeRender(GraphicsResourceAllocator allocator, DeltaTracker tickCounter, boolean renderBlockOutline, Camera camera, Matrix4f positionMatrix, Matrix4f basicProjectionMatrix, Matrix4f projectionMatrix, GpuBufferSlice fogBuffer, Vector4f fogColor, boolean renderSky, CallbackInfo ci) {
 		Execute.beforeLevelRender();
-	}
-
-	@Inject(method = "renderLevel", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/framegraph/FrameGraphBuilder;addPass(Ljava/lang/String;)Lcom/mojang/blaze3d/framegraph/FramePass;"))
-	private void luminance$copyRenderTarget(GraphicsResourceAllocator allocator, DeltaTracker tickCounter, boolean renderBlockOutline, Camera camera, Matrix4f positionMatrix, Matrix4f basicProjectionMatrix, Matrix4f projectionMatrix, GpuBufferSlice fogBuffer, Vector4f fogColor, boolean renderSky, CallbackInfo ci, @Local FrameGraphBuilder frameGraphBuilder, @Local RenderTargetDescriptor renderTargetDescriptor, @Local PostChain postEffectProcessor, @Share("factory") LocalRef<RenderTargetDescriptor> factory) {
-		factory.set(renderTargetDescriptor);
-	}
-
-	// TODO: this probably isnt needed anymore
-	@Inject(method = "renderLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/PostChain;addToFrame(Lcom/mojang/blaze3d/framegraph/FrameGraphBuilder;IILnet/minecraft/client/renderer/PostChain$TargetBundle;)V", ordinal = 1))
-	private void luminance$copyDepth(GraphicsResourceAllocator allocator, DeltaTracker tickCounter, boolean renderBlockOutline, Camera camera, Matrix4f positionMatrix, Matrix4f basicProjectionMatrix, Matrix4f projectionMatrix, GpuBufferSlice fogBuffer, Vector4f fogColor, boolean renderSky, CallbackInfo ci, @Local FrameGraphBuilder frameGraphBuilder, @Share("depthBackup") LocalRef<RenderTarget> depthBackup, @Share("factory") LocalRef<RenderTargetDescriptor> factory) {
-		FramePassInterface.createForcedPass(frameGraphBuilder, Identifier.fromNamespaceAndPath(Data.getVersion().getID(), "copy_depth"), () -> {
-			CrossFrameResourcePool pool = ((GameRendererAccessor)ClientData.minecraft.gameRenderer).getResourcePool();
-			depthBackup.set(pool.acquire(factory.get()));
-			depthBackup.get().copyDepthFrom(targets.main.get());
-		});
-	}
-
-	@Inject(method = "renderLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/PostChain;addToFrame(Lcom/mojang/blaze3d/framegraph/FrameGraphBuilder;IILnet/minecraft/client/renderer/PostChain$TargetBundle;)V", ordinal = 1, shift = At.Shift.AFTER))
-	private void luminance$restoreDepth(GraphicsResourceAllocator allocator, DeltaTracker tickCounter, boolean renderBlockOutline, Camera camera, Matrix4f positionMatrix, Matrix4f basicProjectionMatrix, Matrix4f projectionMatrix, GpuBufferSlice fogBuffer, Vector4f fogColor, boolean renderSky, CallbackInfo ci, @Local FrameGraphBuilder frameGraphBuilder, @Share("depthBackup") LocalRef<RenderTarget> depthBackup, @Share("factory") LocalRef<RenderTargetDescriptor> factory) {
-		FramePassInterface.createForcedPass(frameGraphBuilder, Identifier.fromNamespaceAndPath(Data.getVersion().getID(), "restore_depth"), () -> {
-			CrossFrameResourcePool pool = ((GameRendererAccessor)ClientData.minecraft.gameRenderer).getResourcePool();
-			targets.main.get().copyDepthFrom(depthBackup.get());
-			pool.release(factory.get(), depthBackup.get());
-		});
 	}
 
 	@Inject(method = "renderLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/LevelRenderer;addLateDebugPass(Lcom/mojang/blaze3d/framegraph/FrameGraphBuilder;Lnet/minecraft/client/renderer/state/CameraRenderState;Lcom/mojang/blaze3d/buffers/GpuBufferSlice;Lorg/joml/Matrix4f;)V", shift = At.Shift.AFTER))
